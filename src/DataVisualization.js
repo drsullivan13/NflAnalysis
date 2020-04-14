9cf32da9d13e3322ee433bda822363fffbeea89b
Dan Sullivan <drsullivan13@gmail.com>
2020-04-13 21:34:35 -0400

    Creates api script to start python backend

---
+++ src/DataVisualization.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function DataVisualization(props) {
+    return(
+        <p>
+            {props.message}
+        </p>
+    )
+}
+
+export default DataVisualization;
